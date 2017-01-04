7ddac271c55b3c84d629ed60ca54683905e024a4
Jon Low <lowjonc@gmail.com>
2017-01-04 09:09:56 -0700

    d

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express()
+
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+let messages = []d  
+
+app.get('/messages', function (req, res, next) {
+  res.status(200).json({ messages: messages})
+})
+
+app.post('/messages', function (req, res, next) {
+  // console.log(req.body);
+  messages.push({ message: req.body.message, time: new Date(), user: req.body.userName })
+  res.status(200).json({messages: messages})
+})
+
+
+
+app.listen(3000, function () {
+  console.log('now listening @ port 3000');
+})
